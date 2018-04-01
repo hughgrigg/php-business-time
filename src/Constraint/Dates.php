7972561c49c377b02d3964bc9fca729c82927256
Hugh Grigg <hugh.grigg@gmail.com>
2018-04-01 16:50:34 +0100

    Dates constraint

---
+++ src/Constraint/Dates.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BusinessTime\Constraint;
+
+use DateTime;
+
+/**
+ * Business time constraint that matches based on specific dates.
+ *
+ * e.g.
+ * new Dates('2018-05-23') matches 23rd May 2018 only.
+ *
+ * This uses DateTime parsing, so it should work with any reasonable format for
+ * the given dates.
+ *
+ * @see DatesTest
+ */
+class Dates extends FormatConstraint
+{
+    private const FORMAT = 'Y-m-d';
+
+    /**
+     * @param string ...$dates e.g. '2018-05-23', '2019-04-01'
+     */
+    public function __construct(string ...$dates)
+    {
+        parent::__construct(
+            self::FORMAT,
+            ...array_map(
+                function (string $date): string {
+                    return (new DateTime($date))->format(self::FORMAT);
+                },
+                $dates
+            )
+        );
+    }
+}
