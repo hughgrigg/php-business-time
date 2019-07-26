2f39682e04425edff2473c83d520b1b173929466
Hugh Grigg <hugh.grigg@gmail.com>
2019-07-26 22:02:11 +0100

    Add test cases for LimitedIterator

--- tests/Unit/BusinessTime/IterationLimitTest.php
+++ tests/Unit/BusinessTime/IterationLimitTest.php
@@ -3,6 +3,7 @@
 namespace BusinessTime\Tests\Unit\BusinessTime;
 
 use BusinessTime\BusinessTime;
+use BusinessTime\LimitedIterator;
 use LengthException;
 use PHPUnit\Framework\TestCase;
 use Throwable;
@@ -29,5 +30,35 @@ class IterationLimitTest extends TestCase
         // Then an error should be thrown due to the iteration limit.
         self::assertInstanceOf(LengthException::class, $error);
         self::assertEquals($error->getMessage(), 'Iteration limit of 3 reached.');
+    }
+
+
+    public function testLimitedIterator()
+    {
+        // Given we have a limited iterator;
+        $limit = new LimitedIterator(5);
+
+        // When we iterate within the limit;
+        while ($limit->current() < 5) {
+            $limit->next();
+        }
+
+        // Then the iterator should not throw;
+        self::assertEquals(5, $limit->key());
+
+        // But when we exceed the limit;
+        $limit->rewind();
+        $error = null;
+        try {
+            for ($i = 0; $i < 6; $i++) {
+                $limit->next();
+            }
+        } catch (Throwable $e) {
+            $error = $e;
+        }
+
+        // Then an exception should be thrown.
+        self::assertInstanceOf(LengthException::class, $error);
+        self::assertEquals($error->getMessage(), 'Iteration limit of 5 reached.');
     }
 }
